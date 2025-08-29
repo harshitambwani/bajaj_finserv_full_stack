1ec9fac94ef65c9567849ad574a64aa9b65c82fc
Harshit Ambwani <164780948+harshitambwani@users.noreply.github.com>
2025-08-29 11:25:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Read config from environment variables (safer than hardcoding)
